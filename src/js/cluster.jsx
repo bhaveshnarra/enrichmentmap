d172ac8d1bb8b92b83c9de3e2d9bea790f1bbb9b
bhavesh narra <bhaveshsunny60@yahoo.com>
2017-08-17 23:46:41 +0530

    cose layout table cluster

--- src/js/cluster.jsx
+++ src/js/cluster.jsx
@@ -4,52 +4,51 @@ import Plotly from 'plotly.js/dist/plotly-cartesian';
 const PlotlyComponent = createPlotlyComponent(Plotly);
 
 
-const x = [];
-const y = [];
-const z = [];
-
 export class Cluster extends React.Component {
+  constructor(props) {
+  super(props);
+  this.state = {rows: []};
+  this.loaddata();
+  }
 
   loaddata() {
     for (var i = 0; i < geneCountDF.length; i++) {
-      x.push(geneCountDF[i]["Gene"]);
-      y.push(geneCountDF[i]["Count"]);
+      var ele = {
+        id: i,
+        Gene: geneCountDF[i]["Gene"],
+        Count: geneCountDF[i]["Count"]
+      }
+      this.state.rows.push(ele);
     }
+
   }
 
-  componentDidMount() {
-    this.loaddata();
-  }
 
   render() {
-      let data = [
-        {
-          type: 'bar',      // all "bar" chart attributes: #bar
-          x: x,     // more about "x": #bar-x
-          y: y,     // #bar-y
-          name: 'bar chart example' // #bar-name
-        }
-      ];
-      let layout = {                     // all "layout" attributes: #layout
-        title: 'simple example',  // more about "layout.title": #layout-title
-        xaxis: {                  // all "layout.xaxis" attributes: #layout-xaxis
-          title: 'time',
-          autorange: 'true'
-        },
-        yaxis: {                  // all "layout.xaxis" attributes: #layout-xaxis
-          title: 'time',
-          autorange: 'true'
-        }
-      };
-      let config = {
-        showLink: false,
-        displayModeBar: true
-      };
+    let list = this.state.rows.map(p =>{
+         return (
+              <tr>
+                <td>{p.Gene}</td>
+                <td>{p.Count}</td>
+              </tr>
+         );
+    });
       return (
-        <PlotlyComponent className="whatever" data={data} layout={layout} config={config}/>
+        <table className="table table-bordered">
+          <thead>
+            <tr>
+              <th>ID</th>
+              <th>Clusters</th>
+            </tr>
+          </thead>
+          <tbody>
+            {list}
+          </tbody>
+        </table>
       );
     }
 }
+
 
 var geneCountDF = [
   {
--- src/js/react-cytoscape.jsx
+++ src/js/react-cytoscape.jsx
@@ -34,10 +34,10 @@ const defaultEdgeStyle = {
 const defaultStyle = [defaultNodeStyle, defaultEdgeStyle];
 
 const defaultLayout = {
-  name: 'circle',
+  name: 'cose',
 
   fit: true, // whether to fit the viewport to the graph
-  padding: 30, // the padding on fit
+  padding: 15, // the padding on fit
   boundingBox: undefined, // constrain layout bounds; { x1, y1, x2, y2 } or { x1, y1, w, h }
   avoidOverlap: true, // prevents node overlap, may overflow boundingBox and radius if not enough space
   nodeDimensionsIncludeLabels: false, // Excludes the label when calculating node bounding boxes for the layout algorithm
